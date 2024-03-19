12daf9a46e643689b01db79f133b39f8dfc7a710
s27488 <s27488@pjwstk.edu.pl>
2024-03-19 13:23:04 +0100

    Modification 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("My name is Sasha!");
-Console.WriteLine("I live in Warszaw");
+Console.WriteLine("I live in Warszaw and I am a PJATK student");
